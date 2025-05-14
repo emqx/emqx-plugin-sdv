b81cc6e0c7bf0f07242aa77879e62369b78e32d6
zmstone <zmstone@gmail.com>
2025-05-14 16:26:24 +0200

    ci: initial draft of GitHub workflows

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+VSN="$(grep -o 'plugin_rel_vsn, "[^"]*"' rebar.config | cut -d'"' -f2)"
+
 docker run --rm -it \
   --name emqx \
   -p 1883:1883 \
   -p 18083:18083 \
   -v $(pwd)/_build/default/emqx_plugrel:/opt/emqx/plugins \
-  -e EMQX_PLUGINS__STATES='[{enable = true, name_vsn = "emqx_sdv_fanout-1.0.0"}]' \
+  -e EMQX_PLUGINS__STATES="[{enable = true, name_vsn = \"emqx_sdv_fanout-${VSN}\"}]" \
   emqx/emqx-enterprise:5.8.6 emqx console
